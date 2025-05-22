ea979318f45c57575eeb213ff0087a448fba10db
Mithun Krishna A <mithunkannan06@gmail.com>
2025-05-22 11:41:21 +0530

    added relationship

--- server/models/Memory.js
+++ server/models/Memory.js
@@ -1,21 +1,10 @@
 const mongoose = require('mongoose');
 
 const memorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-}, {
-  timestamps: true
-});
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+}, { timestamps: true });
 
-const Memory = mongoose.model('Memory', memorySchema);
-module.exports = Memory;
+module.exports = mongoose.model('Memory', memorySchema);
